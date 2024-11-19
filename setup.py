123eeb5e61799bf4f86160c81df6b88231dbe553
Sarala9206 <penjarlasarala15@gmail.com>
2024-11-19 13:32:26 +0530

    added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="uk_visa",
+    version="0.0.0",
+    author="sarala",
+    author_email="penjarlasarala15@gmail.com",
+    packages=find_packages()
+)
